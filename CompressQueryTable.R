d303658c4e1d37cf4a784499b02beb07db9b7508
edge-d <55300829+edge-d@users.noreply.github.com>
2022-10-11 10:37:01 -0700

    updated table

--- CompressQueryTable.R
+++ CompressQueryTable.R
@@ -11,27 +11,27 @@ library(rworldmap)
 #   - column 1 contains the longitude in degrees
 #   - column 2 contains the latitude in degrees
 coords2country = function(points)
-{  
+{
   countriesSP <- getMap(resolution='low')
   #countriesSP <- getMap(resolution='high') #you could use high res map from rworldxtra if you were concerned about detail
-  
+
   # convert our list of points to a SpatialPoints object
-  
+
   # pointsSP = SpatialPoints(points, proj4string=CRS(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
-  
+
   #setting CRS directly to that from rworldmap
-  pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(countriesSP)))  
-  
-  
-  # use 'over' to get indices of the Polygons object containing each point 
+  pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(countriesSP)))
+
+
+  # use 'over' to get indices of the Polygons object containing each point
   indices = over(pointsSP, countriesSP)
-  
+
   # return the ADMIN names of each country
-  #indices$ADMIN  
-  #indices$ISO3 # returns the ISO3 code 
+  #indices$ADMIN
+  #indices$ISO3 # returns the ISO3 code
   #indices$continent   # returns the continent (6 continent model)
   #indices$REGION   # returns the continent (7 continent model)
-  
+
   return(list("country"=indices$ADMIN,
        "continent"=indices$REGION))
 }
@@ -76,7 +76,7 @@ allPub <- apply(queryTable, 1, function(x) unname(unlist(x[authorCols])))
 allPub <- apply(allPub, 2, function(x) unlist(x[!is.na(x)]))
 queryTable$auth <- lapply(allPub, function(x) paste0(unlist(x[!is.na(x)]), collapse = ''))
 
-keeps <- c("archiveType", "varTags", "paleoData_variableName", "geo_latitude", 
+keeps <- c("archiveType", "varTags", "paleoData_variableName", "geo_latitude",
            "geo_longitude", "earliestYear", "mostRecentYear", "auth", "datasetId",
            "lipdverseLink", "dataSetName", "country2", "continent")
 
@@ -96,15 +96,10 @@ zipped.csv <- function(df, zippedfile) {
 }
 
 zipped.csv(df=compressedTable, zippedfile = "queryZip")
-#write.csv(compressedTable, file = "C:/Users/dce72/Documents/LiPDsandbox/query/compQuery.csv")
 
-library(git2rdata)
+#filePath <- unzip(paste0(getwd(), "/queryZip.zip"), list = TRUE)$Name
 
-repo <- repository("~/my_git_repo")
-pull(repo)
-write_vc(my_data, file = "rel_path/filename", root = repo, stage = TRUE)
-commit(repo, "My message")
-push(repo)
+
 
 
 
